2350cc54d47b3755cb644698662e5c8693e589df
LJH <ljf135798@naver.com>
2023-07-03 11:09:07 +0900

    1111

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>시1</title>
+</head>
+
+<body>
+
+    <div class="wrap">
+        <header>
+            <div class="inner">
+                <h1>LOGO</h1>
+                <nav>
+                    <ul>
+                        <li><a href="../doc/poem01.html">시01</a> </li>
+                        <li><a href="../doc/poem02.html">시02</a> </li>
+                        <li><a href="../doc/poem03.html">시03</a> </li>
+                        <li><a href="../doc/poem04.html">시04</a> </li>
+                        <li><a href="../doc/poem05.html">시05</a> </li>
+                    </ul>
+
+                </nav>
+
+
+            </div>
+
+        </header>
